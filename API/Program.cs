01a7cc8a0df94e63e2843cee13ad52127dbb97c8
arnoroozin <alirezanoruzinejad@gmail.com>
2024-02-20 00:37:10 +0330

    backend part 2

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Extentions;
+using Application;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,15 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt=>{
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectinstring"));
-});
-builder.Services.AddCors(opt=>opt.AddPolicy("CorsPolicy",policy=>{
-    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-}));
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
